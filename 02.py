4ff60323c1f817c60aa2b5c5cc1ed9c0dbf1f067
dankeyy <dankeyy@protonmail.com>
2021-12-02 10:17:49 +0200

    solved day2

---
+++ 02.py
@@ -0,0 +1,27 @@
+
+# moves = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
+
+
+def operations(path='02.txt'):
+    with open(path) as lines:
+        for op, x in map(str.split, lines):
+            yield op, int(x)
+
+
+def solve(p2):
+    real_depth = aim_depth = pos = 0
+    bsignum = lambda cond: int(cond) or -1 # helper to determine direction
+
+    for op, x in operations():
+        assert op in {'forward', 'up', 'down'}
+
+        if op == 'forward':
+            pos += x
+            if p2: real_depth += aim_depth * x
+
+        else: aim_depth += x * bsignum(op == 'down')
+
+    return (real_depth or aim_depth) * pos
+
+
+print(solve(p2=False), solve(p2=True))
