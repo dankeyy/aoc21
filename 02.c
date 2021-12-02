0a9537c6024d34cc3ac06507fac86907da1f45f3
dankeyy <dankeyy@protonmail.com>
2021-12-02 18:23:11 +0200

    added c impl

---
+++ 02.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    FILE *fp = fopen("02.txt", "r");
+    if (!fp)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    char op[16];
+    int x, pos = 0, aim_depth = 0, real_depth = 0;
+
+    while(1)
+    {
+        fscanf(fp, "%s %d", op, &x);
+
+        if (feof(fp))
+        {
+            break;
+        }
+
+        if(strcmp(op, "forward") == 0)
+        {
+            pos += x;
+            real_depth += aim_depth * x;
+        }
+
+        else if(strcmp(op, "up") == 0)
+        {
+            aim_depth -= x;
+        }
+
+        else if(strcmp(op, "down") == 0)
+        {
+            aim_depth += x;
+        }
+    }
+
+    fclose(fp);
+    printf("%d\n%d\n", aim_depth * pos, real_depth * pos);
+}
