313c9ef8c463818a923d18b2e4341b043585dbc3
dankeyy <dankeyy@protonmail.com>
2021-12-05 23:46:26 +0200

    done with p2

--- 05.hs
+++ 05.hs
@@ -1,28 +1,36 @@
 import Data.List
-import Data.List.Split
 import Data.Maybe
+import Data.List.Split
+import Control.Arrow
+
+type Point = (Int, Int)
+type CountDiagonals = Bool
 
 parse :: String -> [[Int]]
 parse x = map read . splitOn "," <$> splitOn " -> " x
 
+toCoordinateRange :: Bool -> [[Int]] -> Maybe [Point]
+toCoordinateRange countDiagonal [x1:y1:_, x2:y2:_]
+  | not countDiagonal = if x1 == x2 || y1 == y2 then Just base else Nothing
+  | otherwise = Just base
+  where base = zip (range x1 x2) (range y1 y2)
+        range start stop = [start, start + step ..stop]
+          where step = signum $ stop - start
+toCoordinateRange _ _ = Nothing
 
-toCoordinateRange :: [[Int]] -> Maybe [(Int, Int)] --[[3,2], [2,3]] -> Just [(2,2),(2,3),(3,2),(3,3)]
-toCoordinateRange xs | null xs || any null xs = Nothing
-toCoordinateRange [x1:y1:_, x2:y2:_]
-  | x1 /= x2 && y1 /= y2 = Nothing
-  | otherwise            = Just [(x, y) | x <-[bottomX..topX], y <- [bottomY..topY]]
-  where
-    (bottomX, topX) = (min x1 x2, max x1 x2)
-    (bottomY, topY) = (min y1 y2, max y1 y2)
+countOverlap :: [Point] -> Int
+countOverlap = length . filter ((>=2) . length) . group . sort
 
-
-countOverlap :: [(Int, Int)] -> Int
-countOverlap = length . filter ((>1). length) . group . sort
-
-solve :: [String] -> Int
-solve = countOverlap . concat . mapMaybe (toCoordinateRange . parse)
-
-x = ["0,9 -> 5,9", "8,0 -> 0,8", "9,4 -> 3,4", "2,2 -> 2,1", "7,0 -> 7,4", "6,4 -> 2,0", "0,9 -> 2,9", "3,4 -> 1,4", "0,0 -> 8,8", "5,5 -> 8,2"]
+solve :: CountDiagonals -> [String] -> Int
+solve countDiagonal = countOverlap . concat . mapMaybe (toCoordinateRange countDiagonal . parse)
 
 main :: IO ()
-main = print . solve . lines =<< readFile "05.txt"
+main = print . (solve False &&& solve True) . lines =<< readFile "05.txt"
+
+-- (0 0) (1 0) (2 0) (3 0) (4 0)
+-- (0 1) (1 1) (2 1) (3 1) (4 1)
+-- (0 2) (1 2) (2 2) (3 2) (4 2)
+-- (0 3) (1 3) (2 3) (3 3) (4 3)
+-- (0 4) (1 4) (2 4) (3 4) (4 4)
+
+-- x = ["0,9 -> 5,9", "8,0 -> 0,8", "9,4 -> 3,4", "2,2 -> 2,1", "7,0 -> 7,4", "6,4 -> 2,0", "0,9 -> 2,9", "3,4 -> 1,4", "0,0 -> 8,8", "5,5 -> 8,2"]
