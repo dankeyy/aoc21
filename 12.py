9b7747a7160854d25f7d50cb094e14e0ea5bd431
dankeyy <dankeyy@protonmail.com>
2022-01-30 19:42:19 +0200

    solved p1&p2

---
+++ 12.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from collections import defaultdict
+
+
+def dfs(node, twice, graph, seen=frozenset()):
+    if node == 'end':
+        return 1
+
+    if node == 'start' and node in seen:
+        return 0
+
+    if node in seen and node.islower() and twice:
+        return sum(dfs(neighbor, False, graph, seen | {node}) for neighbor in graph[node])
+
+    if node not in seen or node.isupper():
+        return sum(dfs(neighbor, twice, graph, seen | {node}) for neighbor in graph[node])
+
+    return 0
+
+
+def main():
+    m = [tuple(x.split('-')) for x in Path('12.txt').read_text().splitlines()]
+
+    graph = defaultdict(set)
+    for a, b in m:
+        graph[a].add(b)
+        graph[b].add(a)
+
+    print(dfs(node='start', graph=graph, twice=False)) # p1
+    print(dfs(node='start', graph=graph, twice=True)) # p2
+
+
+main()
