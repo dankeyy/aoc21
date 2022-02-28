870c23c77fd60efb0e55544c652851c92b806778
dankeyy <dankeyy@protonmail.com>
2022-02-28 17:56:37 +0200

    clean up

--- 15.py
+++ 15.py
@@ -1,35 +1,28 @@
 from heapq import heappop, heappush
 from pathlib import Path
 
+grid = [[*map(int, list(x))] for x in Path('15.txt').read_text().splitlines()]
 neighbors = lambda x, y: ((x+1, y), (x, y+1), (x-1, y), (x, y-1))
 
 
-def solve(grid: list[list[int]], ratio=5):
-
+def solve(grid, ratio=5):
     w, h = len(grid[0]), len(grid)
     end = (w * ratio -1, h * ratio -1)
-    graph = {
-        (x, y) : grid[x][y]
-        for x in range(h)
-        for y in range(w)
-    }
 
+    graph = {(x, y) : grid[x][y] for x in range(h) for y in range(w)}
+    bounded = lambda x, y: 0 <= x < h*ratio and 0 <= y < w*ratio
     heap = [(0, (0,0))]
     visited = set()
 
     while heap:
         risk_so_far, cell = heappop(heap)
-
         for neighbor in neighbors(*cell):
-            x, y = neighbor
+            if bounded(*neighbor) and neighbor not in visited:
 
-            if not (0 <= x < h*ratio and 0 <= y < w*ratio):
-                continue
+                x, y = neighbor
+                risk = graph.get((x % h, y % w))
+                risk = sum(divmod(x // h + y // w + risk, 10))
 
-            risk = graph.get((x % h, y % w))
-            risk = sum(divmod(x // h + y // w + risk, 10))
-
-            if neighbor not in visited:
                 if neighbor == end:
                     return risk + risk_so_far
 
@@ -37,6 +30,5 @@ def solve(grid: list[list[int]], ratio=5):
                 heappush(heap, (risk + risk_so_far, neighbor))
 
 
-grid = [[*map(int, list(x))] for x in Path('15.txt').read_text().splitlines()]
 print(solve(grid, ratio=1)) # part 1
 print(solve(grid, ratio=5)) # part 2
