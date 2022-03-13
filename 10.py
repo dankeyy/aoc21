1cb718900a6e2e6ee069c77ed23d26042f2c4189
dankeyy <dankeyy@protonmail.com>
2022-03-13 21:50:39 +0200

    refactor

--- 10.py
+++ 10.py
@@ -1,55 +1,51 @@
 import re
 from pathlib import Path
 from functools import reduce
+from statistics import median
 
-lines = [line.strip() for line in Path("10.txt").read_text().splitlines()]
-opening = "({[<"
-pairs = ['()', '{}', '[]', '<>']
 
-incomplete = lambda line: all(x in opening for x in line)
-corrupt = lambda line: not incomplete(line)
+LINES = [line for line in Path("10.txt").read_text().splitlines()]
+
 
 def cleaned(line):
-    return (line if all(p not in line for p in pairs)
+    return (line if all(p not in line for p in ('()', '{}', '[]', '<>'))
         else cleaned(re.sub(r"(\(\))|(\{\})|(\[\])|(\<\>)", "", line)))
 
 
-##################################################################################
+def incomplete(line):
+    return all(x in "({[<" for x in line)
 
-scoresA = {
-    ')': 3,
-    ']': 57,
-    '}': 1197,
-    '>': 25137,
-}
+
+def corrupt(line):
+    return not incomplete(line)
+
 
 def corrupt_score(line):
     line = cleaned(line)
-    return 0 if incomplete(line) else next(scoresA[x] for x in line if x not in opening)
+    scoresA = {
+        ')': 3,
+        ']': 57,
+        '}': 1197,
+        '>': 25137,
+    }
+    return (0 if incomplete(line)
+        else next(scoresA[x] for x in line if x not in "({[<"))
 
-
-def p1(lines):
-    return sum(map(corrupt_score, lines))
-
-##################################################################################
-
-scoresB = {
-    '(': 1,
-    '[': 2,
-    '{': 3,
-    '<': 4,
-
-}
 
 def incomplete_score(line):
     line = cleaned(line)[::-1]
-    return 0 if corrupt(line) else reduce(lambda acc, b: acc * 5 + scoresB[b], line, 0)
+    scoresB = {
+        '(': 1,
+        '[': 2,
+        '{': 3,
+        '<': 4,
+    }
+    return (0 if corrupt(line)
+        else reduce(lambda acc, b: acc * 5 + scoresB[b], line, 0))
 
 
-def p2(lines):
-    scores = sorted(score for line in lines if (score := incomplete_score(line)) != 0)
-    return scores[len(scores) // 2]
+p1 = sum(map(corrupt_score, LINES))
+print(p1)
 
-##################################################################################
-
-print(p1(lines), p2(lines))
+p2 = median(score for line in LINES if (score := incomplete_score(line)) != 0)
+print(p2)
